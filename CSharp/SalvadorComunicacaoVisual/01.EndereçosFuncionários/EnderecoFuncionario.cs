90929b3c3945b3dc36ade442634b082c96540f53
Jherryett Salvador <jherryett@gmail.com>
2025-02-18 11:49:31 -0300

    Atualizando o desenvolvimento do projeto

--- CSharp/SalvadorComunicacaoVisual/01.EndereçosFuncionários/EnderecoFuncionario.cs
+++ CSharp/SalvadorComunicacaoVisual/01.EndereçosFuncionários/EnderecoFuncionario.cs
@@ -4,13 +4,30 @@ namespace SalvadorComunicacaoVisual.EnderecosFuncionarios
     public class EnderecoFuncionario
 
     {
-       public string? IdEnderecoFuncionario { get; set; }
-        public string? NomeRua {  get; set; }
+        public string? IdEnderecoFuncionario { get; set; }
+        public string? NomeRua { get; set; }
         public string? NomeBairro { get; set; }
         public string? NomeCidade { get; set; }
         public string? NomeEstado { get; set; }
         public string? Complemento1 { get; set; }
         public string? Complemento2 { get; set; }
         public DateTime DataCadastro { get; set; }
+
+        public EnderecoFuncionario() { }
+        public EnderecoFuncionario(string id, string nomeRua, string nomeBairro, string nomeCidade, string nomeEstado, string complemento1, string complemento2, DateTime dataCadastro)
+
+        {
+            IdEnderecoFuncionario = id;
+            NomeRua = nomeRua;
+            NomeBairro = nomeBairro;
+            NomeCidade = nomeCidade;
+            NomeEstado = nomeEstado;
+            Complemento1 = complemento1;
+            Complemento2 = complemento2;
+            DataCadastro = dataCadastro;
+        }
+
     }
+
 }
+
---
+++ CSharp/SalvadorComunicacaoVisual/01.EndereçosFuncionários/MockFuncionario.cs
@@ -0,0 +1,32 @@
+﻿using SalvadorComunicacaoVisual.EnderecosFuncionarios;
+using System;
+
+namespace SalvadorComunicacaoVisual.MockFuncionario
+
+{
+    class MockFuncionario // Criei um mock para simular a entrada de dados, para testar service e repository
+    {
+        static void Main()
+        {
+            //Construtor do objeto endereco
+            EnderecoFuncionario endereco = new EnderecoFuncionario();
+
+            Console.WriteLine("_____ Iniciando teste de mock via console _____");
+            Console.WriteLine("Insira os dados para cadastro do endereço do funcionário");
+
+            Console.WriteLine("Digite o nome da rua");
+            endereco.NomeRua = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do bairro");
+            endereco.NomeBairro = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome da Cidade");
+            endereco.NomeCidade = Console.ReadLine();
+
+            Console.WriteLine("Digite o complemento se necessário");
+            endereco.Complemento1 = Console.ReadLine();
+
+
+        }
+    }
+}
